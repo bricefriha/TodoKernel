53374d33e822e9879e95c6891d63fed3ed1066c9
Brice Friha <brice.friha@aol.fr>
2020-06-08 19:45:14 +0200

    reupdate gitignore

--- config/config.js
+++
@@ -1,8 +0,0 @@
-//config/Config.js
-
-// to connect to the database
-module.exports = {
-    DB: process.env.MONGO_URL ? process.env.MONGO_URL : 'mongodb://localhost:27017/todoKernel',
-    APP_PORT: process.env.APP_PORT ? process.env.APP_PORT : 80,
-    Secret: process.env.SECRET ? process.env.SECRET : "[put your secret here]",
-  };
