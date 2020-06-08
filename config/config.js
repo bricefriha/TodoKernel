14b1e25b0aa5e9082db1cab0cfc67af50121209c
Brice Friha <37577669+bricefriha@users.noreply.github.com>
2020-06-08 18:01:39 +0200

    set config.js

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@
 
 // to connect to the database
 module.exports = {
-    DB: process.env.MONGO_URL ? process.env.MONGO_URL : 'mongodb://localhost:27017/briceTodo',
+    DB: process.env.MONGO_URL ? process.env.MONGO_URL : 'mongodb://localhost:27017/todoKernel',
     APP_PORT: process.env.APP_PORT ? process.env.APP_PORT : 80,
-    Secret: process.env.SECRET ? process.env.SECRET : "qsefthukp",
+    Secret: process.env.SECRET ? process.env.SECRET : "[YOUR SECRET]",
   };
