704e9be8a32048ebdb123d1f7c33c670607dfc6c
Brice Friha <brice.friha@aol.fr>
2020-06-09 15:39:20 +0200

    User controllers: error management enhancement

--- controllers/users.js
+++ controllers/users.js
@@ -1,12 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-// Import JWT 
-let jwt = require('jsonwebtoken');
-
-// Call the config file
-const config = require('../config/config.js');
-
 // Import the repository
 const repository = require('../repositories/UserRepository');
 const TodolistRepo = require('../repositories/TodoListRepository');
@@ -17,37 +11,37 @@ const TodolistRepo = require('../repositories/TodoListRepository');
 router.post('/authenticate',(req, res) => {
     repository.authenticate(req.body)
         .then(user => user ? res.json(user) : res.status(400).json({ message: 'Username or password is incorrect' }))
-        .catch(err => res.status(500).json(err));
+        .catch(error => res.status(500).json({status:"Error 500", message: error}));
 });
 
 // Register the user
-router.post('/register', (req, res, next) => {
+router.post('/register', (req, res) => {
     repository.create(req.body)
         .then(user => res.json(user))
-        .catch(err => next(err));
+        .catch(error => res.status(500).json({status:"Error 500", message: error}));
 });
 
 // Get the current user
-router.get('/current', (req, res, next) => {
+router.get('/current', (req, res) => {
     repository.getById(req.user.sub)
         .then(user => user ? res.json(user) : res.sendStatus(404))
-        .catch(err => next(err));
+        .catch(error => res.status(500).json({status:"Error 500", message: error}));
 });
 // Get a single user
-router.get('/:id', (req, res, next) => {
+router.get('/:id', (req, res) => {
     repository.getById(req.params.id)
         .then(user => user ? res.json(user) : res.sendStatus(404))
-        .catch(err => next(err));
+        .catch(error => res.status(500).json({status:"Error 500", message: error}));
 });
 // Update a user
-router.put('/', (req, res, next) => {
+router.put('/', (req, res) => {
 
     repository.update(req.user.sub, req.body)
         .then(updatedUser => res.json(updatedUser))
-        .catch(err => next(err));
+        .catch(error => res.status(500).json({status:"Error 500", message: error}));
 });
 // Delete a user
-router.delete('/current', (req, res, next) => {
+router.delete('/current', (req, res) => {
 
     repository._delete(req.user.sub)
         .then(function() {
@@ -55,17 +49,17 @@ router.delete('/current', (req, res, next) => {
             TodolistRepo.deleteByUser(userId);
             res.json({status: "OK", result:  " deleted"});
         })
-        .catch(err => next(err));
+        .catch(error => res.status(500).json({status:"Error 500", message: error}));
 });
 // Delete a user
-router.delete('/cleanup', (req, res, next) => {
+router.delete('/cleanup', (req, res) => {
     
     // Delete all todolist of the current user
     TodolistRepo.deleteByUser(req.user.sub)
         .then(function() {;
             res.json({status: "OK", result:  " deleted"});
         })
-        .catch(err => next(err));
+        .catch(error => res.status(500).json({status:"Error 500", message: error}));
 });
 
 
