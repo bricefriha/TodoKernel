0eaa17456b4d1b5522e89c4bd37a96582c04b94d
Brice Friha <brice.friha@aol.fr>
2020-06-16 18:50:18 +0200

    I made sure that the todoitem controllers fit the standards of nodejs

--- src/controllers/todos.js
+++ src/controllers/todos.js
@@ -1,11 +1,7 @@
 // Import express
 const express = require('express');
 
-// Import JWT 
-let jwt = require('jsonwebtoken');
-
-const app = express.Router();
-const config = require('../config/config.js');
+const router = express.Router();
 
 // Import the ItemRepo
 const ItemRepo = require('../repositories/TodoItemRepository');
@@ -14,48 +10,48 @@ const TodoListRepo = require('../repositories/TodoListRepository');
 // Routes
 //
 // Add a todo item
-app.post('/:add', (req, res) => {
+router.post('/:add', (req, res) => {
 
     const { name, todolistId } = req.body;
 
     // Add the item to the selected todolist
-    ItemRepo.create(name, todolistId, req.user.sub).then((item) => {
-        TodoListRepo.addItem(item._id, todolistId);
+    ItemRepo.create(name, todolistId, req.user.sub).then((data) => {
+        TodoListRepo.addItem(data._id, todolistId);
          
         // Return this one in Json format
-        res.status(200).json(item);
+        res.status(200).json(data);
 
-    }).catch(error => res.status(500).json(error));
+    }).catch(err => res.status(500).json(err));
 });
 
 // Add a todo item
-app.get('/get', (req, res) => {
+router.get('/get', (req, res) => {
 
     const { todolistId } = req.body;
 
     // Get items from the selected todolist
-    ItemRepo.findByTodolist(todolistId, req.user.sub).then((item) => {
+    ItemRepo.findByTodolist(todolistId, req.user.sub).then((data) => {
          
         // Return this one in Json format
-        res.status(200).json(item);
+        res.status(200).json(data);
 
-    }).catch(error => res.status(500).json(error));
+    }).catch(err => res.status(500).json(err));
 });
 
 // delete a todo item
-app.delete('/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
 
     // Fetch the item id
     const { id } = req.params;
 
-    ItemRepo.delete(id, req.user.sub).then((ok) => {
+    ItemRepo.delete(id, req.user.sub).then((data) => {
       // Return a success
-      res.status(200).json({result: ok, message: `Deleted record with id: ${id}`});
-    }).catch(error => res.status(500).json(error));
+      res.status(200).json({result: data, message: `Deleted record with id: ${id}`});
+    }).catch(err => res.status(500).json(err));
   });
 
 // Update a todo item
-app.put('/rename/:id', (req, res) => {
+router.put('/rename/:id', (req, res) => {
     
     // get the id
     const { id } = req.params;
@@ -65,23 +61,23 @@ app.put('/rename/:id', (req, res) => {
 
     // update it
     ItemRepo.rename(id, req.user.sub, todo)
-        .then(result => result ? res.status(200).json(result) : res.status(403).json({ message: 'item not found' }))
-        .catch(error => res.status(500).json({status:"Error", message: error}));
+        .then(data => data ? res.status(200).json(data) : res.status(403).json({ message: 'item not found' }))
+        .catch(err => res.status(500).json({status:"Error", message: err}));
 });
 
 // check todo item
-app.put('/check/:id', (req, res) => {
+router.put('/check/:id', (req, res) => {
     
     // get the id
     const { id } = req.params;
 
     // update it
     ItemRepo.check(id, req.user.sub)
-        .then(result => result ? res.status(200).json(result) : res.status(403).json({ message: 'item not found' }))
-        .catch(error => res.status(500).json({ status:"Error", message: error }));
+        .then(data => data ? res.status(200).json(data) : res.status(403).json({ message: 'item not found' }))
+        .catch(err => res.status(500).json({ status:"Error", message: err }));
 });
 
-module.exports = app;
+module.exports = router;
 
 
 
