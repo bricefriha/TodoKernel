891b88c8ef0ef103d345c05c513f0095367cf5e3
Brice Friha <brice.friha@aol.fr>
2020-06-22 19:10:39 +0200

    update config template

--- src/config-dist/config.js
+++ src/config-dist/config.js
@@ -2,10 +2,13 @@
 
 // to connect to the database
 module.exports = {
-    DB: process.env.MONGO_URL ? process.env.MONGO_URL : 'mongodb://localhost:27017/todokernel',
+    DB: process.env.MONGO_URL ? process.env.MONGO_URL : 'mongodb://[server]:[port]/[database name]',
     APP_PORT: process.env.APP_PORT ? process.env.APP_PORT : 80,
     Secret: process.env.SECRET ? process.env.SECRET : "[put your secret here]",
     serviceEmail: process.env.SERVICE_EMAIL ? process.env.SERVICE_EMAIL : "[Your email sevice (gmail, outlook, however...)]",
-  Email: process.env.EMAIL ? process.env.EMAIL : "[Your email adress]",
-  PasswordEmail: process.env.PWD_EMAIL ? process.env.PWD_EMAIL : "[Password of your email account]",
+    Email: process.env.EMAIL ? process.env.EMAIL : "[Your email adress]",
+    PasswordEmail: process.env.PWD_EMAIL ? process.env.PWD_EMAIL : "[Password of your email account]",
+
+    // Set a public path
+    publicPath: 'api',
   };
